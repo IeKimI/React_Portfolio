af62e5de12c0df721a248d06be2ae216454abb55
Eri Kim <erikim0413@gmail.com>
2021-06-11 20:08:43 +0200

    Testimonials done

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,57 @@
 import "./testimonials.scss"
 
 export default function Testimonials() {
+
+    const data =[
+        {
+            id: 1,
+            name: "Athina Theofilou",
+            title: "Future Architect",
+            img: "assets/portrait_small.png",
+            icon: "assets/linkedin.png",
+            desc: "eri is good",
+            featured: true
+        },
+        {
+            id: 2,
+            name: "adfa Theofilou",
+            title: "Future Architect",
+            img: "assets/portrait_small.png",
+            icon: "assets/linkedin.png",
+            desc: "eri is good",
+            featured: false
+        },
+        {
+            id: 3,
+            name: "Athidafdafdna Theofilou",
+            title: "Future Architect",
+            img: "assets/portrait_small.png",
+            icon: "assets/linkedin.png",
+            desc: "eri is good",
+            featured: false
+        },
+    ]
     return (
         <div className="testimonials" id="testimonials">
-            
+            <h1>Testimonials</h1>
+            <div className="container">
+                {data.map(d=>(
+                <div className={d.featured ? "card featured" : "card"}>
+                    <div className="top">
+                        <img src="assets/left-arrow.png" className="left" alt="" />
+                        <img src={d.img} className="pic" alt="" />
+                        <img src={d.icon} className="right" alt="" />
+                    </div>
+                    <div className="center">
+                        {d.desc}
+                    </div>
+                    <div className="bottom">
+                        <h3>{d.name}</h3>
+                        <h4>{d.title}</h4>
+                    </div>
+                </div>
+                ))}
+            </div>
         </div>
     )
 }
--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -1,30 +1,67 @@
+import { useState } from "react"
 import "./work.scss"
 
 export default function Work() {
+
+    const [currentSlide, setCurrentSlide] = useState(0)
+
+    const data=[
+        {
+            id: "1",
+            icon: "./assets/linkedin.png",
+            title: "example1",
+            desc: "description",
+            img: "./assets/linkedin.png"
+        },
+        {
+            id: "2",
+            icon: "./assets/linkedin.png",
+            title: "example2",
+            desc: "description",
+            img: "./assets/linkedin.png"
+        },
+        {
+            id: "3",
+            icon: "./assets/linkedin.png",
+            title: "example3",
+            desc: "description",
+            img: "./assets/linkedin.png"
+        },
+    ]
+
+    const handleClick = (way)=> {
+        way === "left" ? setCurrentSlide(currentSlide > 0 ? currentSlide-1 : 2) : 
+        setCurrentSlide(currentSlide < data.length-1 ? currentSlide+1 : 0);
+    }
     return (
         <div className="work" id="work">
 
-            <div className="slider">
-                <div className="container">
-                    <div className="item">
-                        <div className="left">
-                            <div className="leftContainer">
-                                <div className="imgContainer">
-                                    <img src="assets/linkedin.png" alt="" />
+            <div className="slider" style={{ transform:`translateX(-${currentSlide * 100}vw`}}>
+                {data.map((d)=>(
+                    <div className="container">
+                        <div className="item">
+                            <div className="left">
+                                <div className="leftContainer">
+                                    <div className="imgContainer">
+                                        <img src={d.icon} alt="" />
+                                    </div>
+                                    <h2>{d.title}</h2>
+                                    <p>{d.desc}</p>
+                                    <span>Projects</span>
                                 </div>
-                                <h2>Title</h2>
-                                <p>Random sentences</p>
-                                <span>Projects</span>
+                            </div>
+                            <div className="right">
+                                <img src="assets/linkedin.png" alt="" />
+
                             </div>
                         </div>
-                        <div className="right">
-                            <img src="assets/linkedin.png" alt="" />
-
-                        </div>
-                    </div>
-                </div>
+                    
+                </div>))}
+                
             </div>
-            
+            <img src="assets/left-arrow.png" className="arrow left" alt="" onClick={()=>handleClick("left")}/>
+            <img src="assets/left-arrow.png" className="arrow right" alt="" onClick={()=>handleClick()}/>
+
         </div>
     )
 }
